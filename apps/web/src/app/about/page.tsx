a5d372f55f17cb07c2e08f343462a9acaaca23ff
Oliwer <swerk.pro@gmail.com>
2025-07-08 21:57:11 +0200

    Test commit

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import PageLayout from '@/components/page-layout'
+
+export default function About() {
+	return (
+		<PageLayout>
+			<h1 className="text-2xl font-bold">About</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+			</p>
+		</PageLayout>
+	)
+}
